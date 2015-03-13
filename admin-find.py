97e02684edddaa6f14e4a4eb6aea8482f05e58d1
Ialle Ironbits <ialle.ironbits@gmail.com>
2015-03-13 08:09:59 -0300

    Create admin-find.py
    This is the python version of scylla-find3r. We pretend to keep using perl instead of python in the whole project.
    
    This version doesn't have any requirements.

---
+++ admin-find.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+### DIARIOSEC GROUP ###
+### B3RG & IRONBITS ###
+### www.Ironbits.me ###
+import sys
+
+try:
+    import requests
+except Exception:
+    print "You don't have 'requests' library installed. Please run 'easy_install requests' as root to install it."
+    sys.exit(1)
+
+url = sys.argv[1]
+if url.count("http://") <> 1:
+    url = "http://"+url
+
+if len(sys.argv) == 3:
+    print ""
+    print "====== Requesting to %s ======" %(url)
+    print ""
+    f = open(sys.argv[2])
+    for path in f:
+        request = requests.get(url+path)
+        if request.status_code == 200:
+            print path
+    print ""
+    print "====== End requesting to %s ======" %(url)
+    print ""
+else:
+    print "Usage: path_request.py "
+
+sys.exit(0)
